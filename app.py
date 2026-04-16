09cdf918904b6a29545b1df793e3ce812381637c
Zunair Ahmad <125442598+Zunair20@users.noreply.github.com>
2026-04-16 16:41:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-# from flask import Flask
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello():
-#     return "Hello World"
-
-# app.run(host="0.0.0.0", port=5000)
-
-
 import requests
 from flask import Flask
 
